6520bcfcc1c13fee5e715df62df20d61e5cce338
nickyzz1 <nicolle.rodrigues.siqueira@gmail.com>
2025-04-10 17:10:02 -0300

    styling

--- front/src/app/register/page.tsx
+++ front/src/app/register/page.tsx
@@ -3,7 +3,9 @@
 import Image from "next/image";
 import Link from "next/link";
 import logo from '../../../public/assets/logo.png'
-import TextField from '@mui/material/TextField';
+import TextField, { TextFieldProps } from '@mui/material/TextField';
+import { alpha, Button, ButtonProps, InputBase, OutlinedInputProps, Stack, styled } from "@mui/material";
+import { purple } from '@mui/material/colors'
 
 export default function register() {
     return (
@@ -14,31 +16,28 @@ export default function register() {
           <div className="bg-[#9356EA] rounded-4xl w-2/4 h-1/2 min-w-72 py-60 flex justify-center flex-col items-center max-w-xl">
             <div className="w-3/5">
               <p className="text-white my-2">Nome</p>
-              <TextField
-                id="filled-multiline-flexible"
-                multiline
-                maxRows={4}
-                variant="filled"
-                className="bg-white p-2 w-full"
-                sx={{
-                  borderRadius: '12px',
-                }}
-              />
+              <input className="bg-white rounded-2xl p-2 w-full focus:duration-300 focus:ease-in focus:shadow-pink-500 border-pink-400 outline outline-pink-400 shadow-md shadow-pink-400/95" ></input>
+              {/* <input className="bg-white rounded-2xl p-2 w-full outline-0 focus:duration-300 focus:ease-in focus:border-pink-500 focus:outline focus:outline-pink-500 focus:shadow-md focus:shadow-pink-500/95" ></input> */}
             </div>
             <div className="w-3/5">
               <p className="text-white my-2">Email</p>
-              <input className="bg-white rounded-2xl p-2 w-full"></input>
+              <input className="bg-white rounded-2xl p-2 w-full focus:duration-300 focus:ease-in focus:shadow-pink-500 border-pink-400 outline outline-pink-400 shadow-md shadow-pink-400/95" ></input>
             </div>
             <div className="w-3/5">
               <p className="text-white my-2">Senha</p>
-              <input type="password" className="bg-white rounded-2xl p-2 w-full"></input>
+              <input type="password" className="bg-white rounded-2xl p-2 w-full focus:duration-300 focus:ease-in focus:shadow-pink-500 border-pink-400 outline outline-pink-400 shadow-md shadow-pink-400/95" ></input>
             </div>
-            <button className="bg-[#701AE8] hover:bg-[#291d36dc] bg- text-white text-xl rounded-2xl w-40 p-2 my-4 cursor-pointer">Entrar</button>
+              <Button 
+                variant="contained" 
+                style={{ backgroundColor: '#9333ea', color: '#fff', borderRadius: '22px', width : '50%', marginTop : '15px', marginBottom: '15px', padding: '10px' }}
+                >
+                Cadastrar
+              </Button>
             <Link href={"register"} className="text-white">Já tem conta? Logue-se</Link>
           </div>
         </main>
       </div>
-      </>
-    );
-  }
+    </>
+  );
+}
   
